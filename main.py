9fe97e2f20dadccc8d310081de5bbf62603af089
Wellington Menezes <Wellington Menezes>
2024-06-16 12:49:45 -0300

    teste classe de exportação para docx template

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from docxtpl import DocxTemplate, InlineImage  # type: ignore
 from docx.shared import Mm
 
@@ -27,22 +29,66 @@ dict_spec = {
     "Código ANATEL": "ANATEL00011122",
 }
 
-content_specs = []  # type: ignore
-context = {}
-template = DocxTemplate("template_bg_notebook.docx")
+PATH_OUT_DIR = f"{os.path.dirname(os.path.abspath(__file__))}\\tmp"
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+TEMP_DIR = os.path.join(BASE_PATH, "tmp")
+IMAGE_URL = "https://www.parceirosacer.com.br/Conteudos-Especiais/Notebooks/Predator-Helios-Neo/PHN16-71-76PL/PHN16-71-76PL.jpg"
 
-list_specs = [(k, v) for k, v in dict_spec.items()]
 
-for data in list_specs:
-    content_specs.append({"label": data[0], "value": data[1]})
+class SpecificationToDocx:
 
-context["content_specs"] = content_specs
+    _list_specifications = list()
+    _context = dict()
+    _templates_dir = os.path.join(BASE_PATH, "templates")
+    _temp_dir = os.path.join(BASE_PATH, "tmp")
 
-image_bg = InlineImage(
-    template, image_descriptor="PHN16-71-76PL.jpg", width=Mm(100), height=Mm(100)
+    @classmethod
+    def execute(
+        cls,
+        specifications: dict,
+        product_type: str,
+        product_id: str,
+        product_model: str,
+        product_family: str,
+        product_image_name: str,
+    ):
+        template = DocxTemplate(
+            os.path.join(cls._templates_dir, f"tpl_bg_{product_type.lower()}.docx")
+        )
+        specification_to_list = [(k, v) for k, v in specifications.items()]
+
+        for spec in specification_to_list:
+            cls._list_specifications.append({"label": spec[0], "value": spec[1]})
+
+        image_bg = InlineImage(
+            template,
+            image_descriptor=os.path.join(cls._temp_dir, product_image_name),
+            width=Mm(125),
+            height=Mm(125),
+        )
+
+        cls._context["image_bg"] = image_bg
+        cls._context["content_specs"] = cls._list_specifications
+        cls._context["model"] = product_model
+        cls._context["family"] = product_family
+
+        template.render(cls._context)
+        template.save(
+            os.path.join(cls._temp_dir, f"bg_{product_model}_{product_id}.docx")
+        )
+
+
+hero_image = requests.get(IMAGE_URL)
+
+with open(os.path.join(TEMP_DIR, "PHN16-71-76PL.jpg"), "wb") as image:
+    image.write(hero_image.content)
+
+
+SpecificationToDocx.execute(
+    dict_spec,
+    "NOTEBOOK",
+    "6662662662",
+    "PHN16-71-76PL",
+    "Predator Helios Neo",
+    "PHN16-71-76PL.jpg",
 )
-
-context["image_bg"] = image_bg
-
-template.render(context)
-template.save("bg_notebook_out.docx")
